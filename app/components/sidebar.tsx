671037f9120b2ad218906d89d1b9f22a769cec61
期贤 <qixian.cs@outlook.com>
2023-06-29 21:10:23 +0800

    修改文案

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -135,13 +135,6 @@ export function SideBar(props: { className?: string }) {
           onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
           shadow
         />
-        <IconButton
-          icon={<PluginIcon />}
-          text={shouldNarrow ? undefined : Locale.Plugin.Name}
-          className={styles["sidebar-bar-button"]}
-          onClick={() => showToast(Locale.WIP)}
-          shadow
-        />
       </div>
 
       <div
